9aab6f9896ad342934ba5505039b52b2bd575bdd
zengjunfeng <zengjunfeng@yinmaisoft.com>
2024-09-10 19:01:44 +0800

    更新配置

--- uniCloud-aliyun/cloudfunctions/subscribeMessage/index.obj.js
+++ uniCloud-aliyun/cloudfunctions/subscribeMessage/index.obj.js
@@ -13,7 +13,7 @@ module.exports = {
 		} = data
 		// 初始化实例
 		let uniSubscribemsg = new UniSubscribemsg({
-			dcloudAppid: "__UNI__C420FBA",
+			dcloudAppid: "__UNI__CBF6C30",
 			provider: "weixin-mp",
 		});
 		// 发送订阅消息
